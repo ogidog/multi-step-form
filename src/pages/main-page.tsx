7f7954f5f0c205824fb04506ace2b9814075fc0e
ogidog <ogidog@mail.ru>
2023-05-05 23:26:25 +0700

    Issue with phone number "+" sign leading (Solved)

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -5,11 +5,15 @@ import {Container, Footer, Main, Sidebar} from "widgets/index";
 
 const Page = styled.div`
   min-width: 100vw;
-  height: 100vh;
+  
+  @media(min-width: 1024px){
+    height: 100vh; 
+  }
   
   @media (max-width: 1023px) {
     min-height: 746px;
     max-height: 100%;
+    height: 100dvh;
     overflow-y: auto;
   }
 
